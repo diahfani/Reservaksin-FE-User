f269c20b3f0566ede108bdf957f292c5b2e88677
dwisabrina23 <dwisabrina10@gmail.com>
2021-12-17 01:17:00 +0800

    feat: fix login validation

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import './login.css';
-import {useDispatch} from "react-redux";
-import {login} from "../../Config/Redux/LoginSlice";
+import "./login.css";
+import { useDispatch } from "react-redux";
+import { login } from "../../Config/Redux/LoginSlice";
+import  {Toaster} from "react-hot-toast";
+import { ToastError} from "../Toast/Toast";
 
 function LoginForm(props) {
   const navigate = useNavigate();
@@ -22,7 +24,8 @@ function LoginForm(props) {
   const [errMsg, setErrMsg] = useState(formError);
 
   //regex for validation
-  const isEmail=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;  
+  const isEmail =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const isNIK =
     /^(1[1-9]|21|[37][1-6]|5[1-3]|6[1-5]|[89][12])\d{2}\d{2}([04][1-9]|[1256][0-9]|[37][01])(0[1-9]|1[0-2])\d{2}\d{4}$/;
 
@@ -32,18 +35,29 @@ function LoginForm(props) {
     if (name === "username") {
       if (isEmail.test(value) || isNIK.test(value)) {
         setErrMsg({ ...formError, username: "" });
-      }else{
-          if(isNaN(value)){
-            setErrMsg({ ...formError, username: "email salah" });
-          }else{
-            setErrMsg({ ...formError, username: "nik salah" });
-          }
+      } else {
+        if (isNaN(value)) {
+          setErrMsg({ ...formError, username: "email yang Anda masukkan salah" });
+        } else {
+          setErrMsg({ ...formError, username: "NIK yang anda masukkan salah" });
+        }
       }
-    //validate password
-    if (name === "password" && value !== "") {
-      setErrMsg({ ...formError, password: "" });
+      //validate password
+      if (name === "password" && value !== "") {
+        setErrMsg({ ...formError, password: "" });
+      }
     }
-  }};
+  };
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    validateFormValue(name, value);
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
 
   const validateOnSubmit = () => {
     setErrMsg(() => {
@@ -67,37 +81,30 @@ function LoginForm(props) {
     });
   };
 
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    validateFormValue(name, value);
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
-
   const handleSubmit = (event) => {
     event.preventDefault();
-    const validForm = Object.keys(form).filter(
-      (key) => form[key] !== ""
-    );
-    console.log(validForm);
-    if (validForm.length === 2) {
+    const validForm = Object.keys(form).filter((key) => form[key] !== "");
+
+    if (validForm.length < 2) {
+      validateOnSubmit();
+    } else {
+      console.log(errMsg)
+      if(errMsg.username !== "" || errMsg.password !== ""){
+        ToastError("masih ada data yg salah!")
+        return;
+      }
       const loginData = {
         username: form.username,
         login: true,
-      }
-      console.log("log in success")
-      dispatch(login(loginData))
+      };
+      dispatch(login(loginData));
       navigate("/profile");
-    } else {
-      validateOnSubmit();
     }
   };
 
   return (
     <div className="px-3">
+      <Toaster/>
       <form className="form needs-validation pt-5" onSubmit={handleSubmit}>
         <div className="mb-3 has-validation">
           <label htmlFor="username" className="form-label">
@@ -132,17 +139,22 @@ function LoginForm(props) {
         </div>
         <div className="form-group d-flex justify-content-between mb-3">
           <label className="form-remember">
-            <input type="checkbox"/> Ingat saya
-          </label><a className="form-recovery" href="/">Lupa password?</a>
+            <input type="checkbox" /> Ingat saya
+          </label>
+          <a className="form-recovery" href="/">
+            Lupa password?
+          </a>
         </div>
-        <button className="btn btn-primary w-100" type="submit">Login</button>
+        <button className="btn btn-primary w-100" type="submit">
+          Login
+        </button>
       </form>
       <div className="text-center btnact-container">
-        <hr/>
-        <p className="text-unregis">
-          Belum terdaftar?
-        </p>
-        <button className="btn btn-outline-primary mb-3 w-100">Daftar sekarang</button>
+        <hr />
+        <p className="text-unregis">Belum terdaftar?</p>
+        <button className="btn btn-outline-primary mb-3 w-100">
+          Daftar sekarang
+        </button>
       </div>
     </div>
   );
