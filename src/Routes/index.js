6d6edf280d49547a4355de3166d41bc14aa9e868
dwisabrina23 <dwisabrina10@gmail.com>
2021-12-16 16:34:15 +0800

    route: add profile to route

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import {Routes, Route, BrowserRouter} from "react-router-dom"
+import Navbar from '../Components/Navbar'
+
 import MyReservation from '../Pages/MyReservation/MyReservation'
 import Reservation from '../Pages/Reservation/Reservation'
 import UserProfilePage from '../Pages/UserProfilePage/UserProfilePage'
-import Navbar from '../Components/Navbar'
 import Login from '../Pages/LoginPage/Login'
+import PersonalData from '../Pages/PersonalDataPage/PersonalData'
+
 function Router() {
     return (
         <BrowserRouter>
@@ -13,6 +16,7 @@ function Router() {
                 <Route path="/reservasi-vaksin" element={<><Reservation/></>}></Route>
                 <Route path="/my-reservation" element={<><MyReservation/></>}></Route>
                 <Route path="/login" element={<><Navbar/><Login/></>}></Route>
+                <Route path="/profile" element={<><Navbar/><PersonalData/></>}></Route>
             </Routes>
         </BrowserRouter>
     )
