5967f3ba13905e5fb7d07f60240d13762c7487fc
Fadil Ardiansyah <fadhilansyah25@gmail.com>
2021-12-17 17:58:34 +0700

    comp: login-page change filename to Capilize Case & edit index.css

--- src/Pages/LoginPage/Login.jsx
+++ src/Pages/LoginPage/Login.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import LoginForm from "../../Components/LoginForm/LoginForm";
 import "./Login.css";
-import "../Index.css";
 import BrandLogo from "../../Components/BrandLogo/BrandLogo";
 
 function Login() {
