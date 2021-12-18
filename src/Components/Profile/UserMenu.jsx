0b9453b67ff7b4d557da47eca0597317174b2308
dwisabrina23 <dwisabrina10@gmail.com>
2021-12-19 00:59:23 +0800

    feat: update logout feature

--- src/Components/Profile/UserMenu.jsx
+++ src/Components/Profile/UserMenu.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import "./UserProfile.css";
 import { Link } from "react-router-dom";
-import { RiFolderHistoryLine,  } from "react-icons/ri";
-import {TiGroupOutline} from "react-icons/ti";
-import {BsFillPersonLinesFill} from "react-icons/bs";
-import '../../Pages/index.css';
+import { RiFolderHistoryLine } from "react-icons/ri";
+import { TiGroupOutline } from "react-icons/ti";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../../Config/Redux/LoginSlice";
 
 function UserMenu(props) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    dispatch(logout());
+    if (window.location.pathname.includes("profile")) {
+      navigate("/");
+    } else {
+      window.location.reload();
+    }
+  };
+
   return (
-      <>
+    <>
       <div className="container-fluid item-container">
         <div className="item-menu mb-3 p-3">
           <Link to="/profile">
@@ -17,20 +31,22 @@ function UserMenu(props) {
           </Link>
         </div>
         <div className="item-menu mb-3 p-3">
-          <Link to="/profile">
+          <Link to="/profile/id/family">
             <TiGroupOutline />
             <span className="text-menu">Family Member</span>
           </Link>
         </div>
         <div className="item-menu mb-3 p-3">
-          <Link to="/profile">
+          <Link to="/profile/id/history">
             <RiFolderHistoryLine />
             <span className="text-menu">Histori</span>
           </Link>
         </div>
-        <button className="btn btn-primary mt-3">Logout</button>
+        <button className="btn btn-primary mt-3 btn-logout" onClick={onClick}>
+          Logout
+        </button>
       </div>
-      </>
+    </>
   );
 }
 
