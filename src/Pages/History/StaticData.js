123638e23de62d6e53a5d0909100ee84c96264c6
dwisabrina23 <dwisabrina10@gmail.com>
2021-12-19 00:59:52 +0800

    comp: add temp static data

---
+++ src/Pages/History/StaticData.js
@@ -0,0 +1,20 @@
+export const DataHistory =[
+    {
+        nama:"Dwi Sabrina",
+        status:"Selesai",
+        lokasi:"Rs Mangusada",
+        tanggal:"18-12-2021",
+        waktu:"10:00 - 12:00",
+        vaksin:"Astra Zeneca",
+        dosis:1,
+    },
+    {
+        nama:"Dwi Sabrina",
+        status:"Menunggu",
+        lokasi:"Rs Mangusada",
+        tanggal:"11-01-2022",
+        waktu:"10:00 - 12:00",
+        vaksin:"Astra Zeneca",
+        dosis:2,
+    }
+]
