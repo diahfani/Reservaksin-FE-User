e41a29877bbbd284590010f0626525b6f43543b5
dwisabrina23 <dwisabrina10@gmail.com>
2021-12-19 00:56:03 +0800

    route: update history routing

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -7,18 +7,22 @@ import Reservation from '../Pages/Reservation/Reservation'
 import UserProfilePage from '../Pages/UserProfilePage/UserProfilePage'
 import Login from '../Pages/LoginPage/Login'
 import PersonalData from '../Pages/PersonalDataPage/PersonalData'
+import FamilyMember from '../Pages/FamilyMemberList/FamilyMember';
+import History from '../Pages/History/History';
 
 function Router() {
     const isLogged = useSelector((state) => state)
-    
+    console.log("isi use selector", isLogged)
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/profile" element={<><Navbar isLoggedIn={true}/><UserProfilePage/></>}></Route>
+                <Route path="/profile" element={<><UserProfilePage/></>}></Route>
                 <Route path="/reservasi-vaksin" element={<><Reservation/></>}></Route>
                 <Route path="/my-reservation" element={<><MyReservation/></>}></Route>
                 <Route path="/login" element={<><Navbar/><Login/></>}></Route>
                 <Route path="/profile/id/personal" element={<><Navbar/><PersonalData/></>}></Route>
+                <Route path="/profile/id/family" element={<><FamilyMember/></>}></Route>
+                <Route path="/profile/id/history" element={<><Navbar/><History/></>}></Route>
             </Routes>
         </BrowserRouter>
     )
